427d4d36b513c568f38495fa163553dc1a2d6484
SaeyonSivakumaran <saeyon2000@gmail.com>
2019-11-25 20:53:45 -0500

    Added access to Postman API documentation through a redirect controller.

---
+++ src/main/java/PaymentModernizationAPI/Documentation/DocumentationController.java
@@ -0,0 +1,23 @@
+package PaymentModernizationAPI.Documentation;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Controller for documentation
+ */
+@RestController
+@RequestMapping("/api-docs")
+public class DocumentationController {
+
+    /**
+     * Redirect method to access Postman API documentation
+     * @return Postman API documentation
+     */
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public ModelAndView method() {
+        return new ModelAndView("redirect:https://documenter.getpostman.com/view/9582175/SW7dWSQp?version=latest");
+    }
+}
